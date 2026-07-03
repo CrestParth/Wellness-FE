9990b5439c8dd4932068cff6043eff843296e5fd
Rudraksh-2025 <rud.crestcoder@gmail.com>
2026-07-03 17:35:46 +0530

    issue fix

--- src/common/FormValidation.jsx
+++ src/common/FormValidation.jsx
@@ -79,13 +79,13 @@ export const addInstructorValidation = yup.object({
     lastName: yup.string()
         .required("Last name is required"),
 
-    email: yup.string()
-        .email("Invalid email")
-        .required("Email is required"),
+    // email: yup.string()
+    //     .email("Invalid email")
+    //     .required("Email is required"),
 
-    password: yup.string()
-        .required("Password is required")
-        .min(6, "Password must be at least 6 characters"),
+    // password: yup.string()
+    //     .required("Password is required")
+    //     .min(6, "Password must be at least 6 characters"),
 
     // displayName: yup.string()
     //     .required("Display name is required"),
